e42161c2e511ebaabcd7130f99360bd226f0dc76
prayag1740 <prayag.bhatia2015@vit.ac.in>
2025-01-26 12:06:39 -0700

    logger & middleware

--- server.go
+++ server.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"io"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/prayag1740/golang-project/controller"
+	"github.com/prayag1740/golang-project/middleware"
 	"github.com/prayag1740/golang-project/service"
+	"github.com/tpkeeper/gin-dump"
 )
 
 var videoService service.VideoService = service.New()
 var videoController controller.VideoController = controller.New(videoService)
 
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
 func main() {
-	server := gin.Default()
+	server := gin.New()
+
+	setupLogOutput()
+
+	server.Use(gin.Recovery(), middleware.Logger(), middleware.BasicAuth(), gindump.Dump())
 
 	server.GET("/get-video", func(ctx *gin.Context) {
 		ctx.JSON(200, videoController.FindAll())
